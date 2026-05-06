5aa3ddeacf6d164b642223103b21c2a544b98d2a
Jgohnert <goh22001@byui.edu>
2026-05-06 14:24:39 -0600

    recreated cms project as standalone false

---
+++ cms-app/src/app/contacts/contact-item/contact-item.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contact-item',
+  standalone: false,
+  templateUrl: './contact-item.component.html',
+  styleUrl: './contact-item.component.css',
+})
+export class ContactItemComponent {}
