6a964295e4a27ca9e10bd85ea88379fb1cf7743b
Jgohnert <goh22001@byui.edu>
2026-05-20 10:54:38 -0600

    completed the services assignment

---
+++ w05-services-assignment/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { UserService } from './users.service';
+
+@Component({
+  selector: 'app-root',
+  standalone: false,
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+  providers: [UserService]
+})
+export class AppComponent {
+  
+
+}
