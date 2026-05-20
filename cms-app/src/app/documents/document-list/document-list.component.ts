706b0d1c231087c6eb75bfdd776e565de37ec094
Jgohnert <goh22001@byui.edu>
2026-05-20 14:31:38 -0600

    implamented the document service

--- cms-app/src/app/documents/document-list/document-list.component.ts
+++ cms-app/src/app/documents/document-list/document-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, Output, EventEmitter } from '@angular/core';
 import { Document } from '../document.model'
+import { DocumentService } from '../document';
 
 @Component({
   selector: 'cms-document-list',
@@ -10,38 +11,16 @@ import { Document } from '../document.model'
 export class DocumentListComponent {
   @Output() selectedDocumentEvent = new EventEmitter<Document>();
   
-  documents: Document[] = [
-    new Document(
-      1,
-      'name',
-      'description of the document goes here',
-      'file/document',
-      ['null']
-    ),
-    new Document(
-      2,
-      'name2',
-      'description of document number 2 goes here',
-      'file/document2',
-      ['null']
-    ),
-    new Document(
-      3,
-      'name3',
-      'description of document number 3 goes here',
-      'file/document3',
-      ['null']
-    ),
-    new Document(
-      4,
-      'name4',
-      'description of document number 4 goes here',
-      'file/document4',
-      ['null']
-    ),
-  ];
+  documents: Document[] = []
+
+  constructor(public documentService: DocumentService) {}
+
+  ngOnInit() {
+    this.documents = this.documentService.getDocuments();
+  }
 
   onSelectedDocument(document: Document) {
    this.selectedDocumentEvent.emit(document);
+   
   }
 }
